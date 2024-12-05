9a73a37b3f5690ea289d409211f8680673f7e613
Aidan Zhang <aidanzhang06@gmail.com>
2024-12-05 17:43:35 -0500

    debug 1

--- rope.py
+++ rope.py
@@ -69,6 +69,7 @@ def apply_rotary_emb(
     angles = pos[:, None] * freqs[None, :]
     cos = torch.cos(angles)
     sin = torch.sin(angles)
+    print("angles shape:", angles.shape)
     cos = cos[None, :, None, :].expand(query_real.shape[0], -1, query_real.shape[2], -1)
     sin = sin[None, :, None, :].expand(query_real.shape[0], -1, query_real.shape[2], -1)
 

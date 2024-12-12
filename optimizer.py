66bddbf524ee1e3c69624dbdab2b5aeaae5e38a6
Aidan Zhang <aidanzhang06@gmail.com>
2024-12-12 17:00:12 -0500

    debug adam 6

--- optimizer.py
+++ optimizer.py
@@ -67,7 +67,7 @@ class AdamW(Optimizer):
                 a_t = alpha * math.sqrt(1-beta2**t) / (1-beta1**t)
 
                 # Update parameters
-                p.data.add(a_t * m / (torch.sqrt(v) + eps))
+                p.data.add_(a_t * m / (torch.sqrt(v) + eps))
 
                 # Add weight decay after the main gradient-based updates.
                 # Please note that the learning rate should be incorporated into this update.
